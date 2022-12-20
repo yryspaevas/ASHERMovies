65f5f83ff522945ea72f79dfd970c8d944c0c12a
eles <yryspaeva22@gmail.com>
2022-12-20 12:55:00 +0600

    likes - dislikes

--- review/urls.py
+++ review/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CommetViewSet, CreateRatingAPIView,favourite
+from .views import CommetViewSet, CreateRatingAPIView,favourite, like_or_dislike, user_like
 
 
 router = DefaultRouter()
@@ -9,5 +9,7 @@ router.register('comments', CommetViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('rating/', CreateRatingAPIView.as_view()),
-    path('fav/', favourite)
+    path('fav/', favourite),
+    path('like-or-dislike/', like_or_dislike),
+    path('user-like/', user_like)
 ]
